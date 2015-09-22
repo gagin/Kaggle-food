5e4a7fe831e8d829110fb6c623dd19657db0d185
gagin <alexander@gagin.ru>
2015-09-22 01:56:24 -0700

    Fastest way to expand seems to be via anon function

--- expand-grid.R
+++ expand-grid.R
@@ -1,21 +1,10 @@
 library(RJSONIO)
-library(dplyr)
-library(data.table)
+library(magrittr)
 setwd(file.path(normalizePath("~"),"kaggle-food"))
 #f <- fromJSON("train.json")
 f.full <- fromJSON("train.json", simplifyV=F)
-f <- f.full#[1:1000]
-atta.constr <- function(x) {
-        full <- data.frame(matrix(nrow=0, ncol=3))
-        add <- function(x) {        
-        full <<- rbind(full,expand.grid(x))
-        }
-        get <- function() return(full)
-        list(add=add, get=get)
-}
-atta <- atta.constr()
-
+data <- f.full#[1:1000]
 system.time({
-blowup <- lapply(f, atta$add)
-full <- atta$get()
+        data %<>% lapply(expand.grid) %>% (function(x)x[[1]])
 })
+# 11 sec
