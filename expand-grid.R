0b539b624eca21d9deee6cb9abbff437a9791818
gagin <alexander@gagin.ru>
2015-09-22 02:05:23 -0700

    Mistake found. No matter. Expand.grid() is slow anyway.

--- expand-grid.R
+++ expand-grid.R
@@ -5,6 +5,8 @@ setwd(file.path(normalizePath("~"),"kaggle-food"))
 f.full <- fromJSON("train.json", simplifyV=F)
 data <- f.full#[1:1000]
 system.time({
-        data %<>% lapply(expand.grid) %>% (function(x)x[[1]])
+        data %<>% lapply(expand.grid) 
+# %>% (function(x)x[[1]]) This was wrong, only first elements was used
+# but anyway, expand.grid is slower than what I did it unlist variant
 })
 # 11 sec
